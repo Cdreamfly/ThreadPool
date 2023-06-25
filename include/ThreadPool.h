a9cb745926359d678c11b9f2f8761ab17e1c00be
Cdreamfly <2290660986@qq.com>
2023-06-26 00:14:53 +0800

    add start

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -6,44 +6,58 @@
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
+#include <thread>
 
 namespace cm {
 
-    class Task {
-    public:
-        //自动逸任务类型，从Task继承，重写run方法，实现自定义任务处理
-        virtual void run() = 0;
-    };
+	class Task {
+	public:
+		//自动逸任务类型，从Task继承，重写run方法，实现自定义任务处理
+		virtual void run() = 0;
+	};
 
-    enum class PoolMode {
-        MODE_FIXED,     //线程数量固定模式
-        MODE_CACHED     //线程数量动态增长模式
-    };
+	enum class PoolMode {
+		MODE_FIXED,     //线程数量固定模式
+		MODE_CACHED     //线程数量动态增长模式
+	};
 
-    class Thread {
-    public:
-    private:
-    };
+	class Thread {
+	public:
+		using ThreadFunc = std::function<void()>;
+		explicit Thread(const ThreadFunc&);
+		virtual ~Thread();
+		void start();
+	private:
+		ThreadFunc  func_;
+	};
 
-    class ThreadPool {
-    public:
-        ThreadPool() = default;
+	class ThreadPool {
+	public:
+		ThreadPool();
 
-        virtual ~ThreadPool();
+		virtual ~ThreadPool();
 
-        void setMode(const PoolMode &mode);
+		//设置线程池的工作模式
+		void setMode(const PoolMode &mode);
 
-        void start();
+		//设置任务队列数量阈值
+		void setTaskQueueMaxHold(std::size_t);
 
-    private:
-        PoolMode poolMode_{};             //当前线程池的工作模式
-        std::vector<Thread *> thread_;  //线程列表
-        std::size_t initThreadSize_{};    //初始线程数量
-        std::queue<std::shared_ptr<Task>> taskQueue_;    //任务队列
-        std::atomic_uint taskSize_{};     //任务数量
-        std::size_t taskQueueMaxHold_{};  //任务队列数量阈值
-        std::mutex taskQueueMtx_;       //保证任务队列线程安全
-        std::condition_variable notEmpty_;  //任务队列不空
-        std::condition_variable notFull_;   //任务队列不满
-    };
+		void submitTask(const std::shared_ptr<Task> &);
+
+		//开启线程池
+		void start(std::size_t size = std::thread::hardware_concurrency());
+	private:
+		void threadFunc();
+	private:
+		PoolMode poolMode_{};             //当前线程池的工作模式
+		std::vector<Thread *> threads_;  //线程列表
+		std::size_t initThreadSize_{};    //初始线程数量
+		std::queue<std::shared_ptr<Task>> taskQueue_;    //任务队列
+		std::atomic_uint taskSize_{};     //任务数量
+		std::size_t taskQueueMaxHold_{};  //任务队列数量阈值
+		std::mutex taskQueueMtx_;       //保证任务队列线程安全
+		std::condition_variable notEmpty_;  //任务队列不空
+		std::condition_variable notFull_;   //任务队列不满
+	};
 }
--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,12 +1,47 @@
+#include <functional>
 #include "ThreadPool.h"
 
+const std::size_t TASK_MAX_THREAD_HOLD = 1024;
 
-void cm::ThreadPool::setMode(const cm::PoolMode &mode) {
-    poolMode_ = mode;
-}
-
-void cm::ThreadPool::start() {
-
-}
+cm::ThreadPool::ThreadPool() : poolMode_(cm::PoolMode::MODE_FIXED),
+                               initThreadSize_(std::thread::hardware_concurrency()),
+                               taskSize_(0),
+                               taskQueueMaxHold_(TASK_MAX_THREAD_HOLD) {}
 
 cm::ThreadPool::~ThreadPool() = default;
+
+void cm::ThreadPool::setMode(const cm::PoolMode &mode) {
+	poolMode_ = mode;
+}
+
+void cm::ThreadPool::setTaskQueueMaxHold(const std::size_t size) {
+	this->taskQueueMaxHold_ = size;
+}
+
+void cm::ThreadPool::submitTask(const std::shared_ptr<Task> &) {
+
+}
+
+void cm::ThreadPool::start(const std::size_t size) {
+	this->initThreadSize_ = size;
+	for (int i = 0; i < initThreadSize_; ++i) {
+		threads_.emplace_back(new Thread([this] { threadFunc(); }));
+	}
+	for (int i = 0; i < initThreadSize_; ++i) {
+		threads_[i]->start();
+	}
+}
+
+void cm::ThreadPool::threadFunc() {
+
+}
+
+void cm::Thread::start() {
+
+}
+
+cm::Thread::Thread(const cm::Thread::ThreadFunc &func) {
+	this->func_ = func;
+}
+
+cm::Thread::~Thread() = default;
